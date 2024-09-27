b1b8b030054053bc394d2ac0c6839776def8b005
MariaMsschs <maria.msschs@gmail.com>
2024-09-27 13:14:42 -0300

    primeiro

---
+++ src/main/java/com/example/sitepratica/SitePraticaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sitepratica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitePraticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitePraticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sitepratica/SitePraticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sitepratica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitePraticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
